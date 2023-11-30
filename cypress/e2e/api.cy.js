a96e234f271fd0f4afbb4adf1bb546e8eafe8f32
mariiakhairi <mariia_khairi@inbox.ru>
2023-11-30 12:36:59 +0300

    validate headers test

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('REST api testing with cypress', () => {
+	it('Validate headers', () => {
+		cy.request('/users/2').as('user')
+		cy.get('@user')
+			.its('headers')
+			.its('content-type')
+			.should('include', 'application/json')
+
+		cy.get('@user')
+			.its('headers')
+			.its('connection')
+			.should('include', 'keep-alive')
+	})
 })
